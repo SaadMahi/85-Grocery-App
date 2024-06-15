d5d34f64a54fd7ea685c535c561ccacd8327d29d
SaadMahi <saadmahimkar22@gmail.com>
2024-06-15 08:44:00 +0530

    👽️ feat: fetched api from strapi and added categories

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
@@ -10,8 +12,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import GlobalApi from "../_utils/GlobalApi";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [categoryList, setCategoryList] = useState([]);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((res) => {
+      setCategoryList(res.data.data);
+    });
+  };
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
   return (
     <header className="flex justify-between p-5 shadow-sm">
       <div className="flex items-center gap-8">
@@ -21,6 +37,7 @@ const Header = () => {
             width={50}
             height={70}
             alt="grocery-store-logo"
+            priority
           />
           <div className="flex flex-col">
             <span className="text-xl font-bold text-orange-500">Grocery</span>
@@ -38,10 +55,29 @@ const Header = () => {
           <DropdownMenuContent>
             <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
+
+            {categoryList.map((category, index) => (
+              <DropdownMenuItem
+                key={index}
+                className="flex cursor-pointer gap-2"
+              >
+                <Image
+                  unoptimized={true}
+                  src={
+                    process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+                    category.attributes.icon.data[0].attributes.url
+                  }
+                  alt="icon"
+                  width={30}
+                  height={30}
+                />
+                <h1 className="text-lg">{category.attributes.name}</h1>
+              </DropdownMenuItem>
+            ))}
+
+            {/* <DropdownMenuItem>Billing</DropdownMenuItem>
             <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem> */}
           </DropdownMenuContent>
         </DropdownMenu>
 
